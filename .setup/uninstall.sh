620e7753ada0aa04a73871f52ea8d2a2625a9d59
ABHacker Official <63346676+abhackerofficial@users.noreply.github.com>
2020-06-01 21:48:28 +0530

    Update uninstall.sh

--- .setup/uninstall.sh
+++ .setup/uninstall.sh
@@ -1,1 +1,18 @@
+echo -n "Are you sure (Y/N)? "
+read ask
 
+if [[ $input = "1" ]] || [[ $input = "01" ]]
+       then clear
+if [ -d "/data/data/com.termux/files/usr/opt/metasploit-framework" ]; then
+pkg uninstall metasploit -y > /dev/null 2>&1
+echo -e '\033[1;92m[√] Metasploit-framework Uninstalled Successfully. (:\033[0m'
+fi
+
+if [ ! -d "/data/data/com.termux/files/usr/opt/metasploit-framework" ]; then
+echo -e '\033[1;92m[x] Metasploit-framework Uninstalling Failed! );. (:\033[0m'
+echo -e '\033[1;91m[!] metasploit package not found.\033[0m'
+exit
+fi
+
+elif [[ $input = "1" ]] || [[ $input = "01" ]]
+        then exit
