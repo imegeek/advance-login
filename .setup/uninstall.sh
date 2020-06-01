4d9fba45b3548b8ef35256aca68ffd90a6e4ba8a
ABHacker Official <63346676+abhackerofficial@users.noreply.github.com>
2020-06-01 21:49:31 +0530

    Update uninstall.sh

--- .setup/uninstall.sh
+++ .setup/uninstall.sh
@@ -1,7 +1,7 @@
 echo -n "Are you sure (Y/N)? "
 read ask
 
-if [[ $input = "1" ]] || [[ $input = "01" ]]
+if [[ $input = "Y" ]] || [[ $input = "y" ]]
        then clear
 if [ -d "/data/data/com.termux/files/usr/opt/metasploit-framework" ]; then
 pkg uninstall metasploit -y > /dev/null 2>&1
@@ -14,5 +14,5 @@ echo -e '\033[1;91m[!] metasploit package not found.\033[0m'
 exit
 fi
 
-elif [[ $input = "1" ]] || [[ $input = "01" ]]
+elif [[ $input = "N" ]] || [[ $input = "n" ]]
         then exit
