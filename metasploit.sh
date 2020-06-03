df2f5a93f57f83479e233365019c31a2b77b7808
ABHacker Official <63346676+abhackerofficial@users.noreply.github.com>
2020-06-03 17:47:11 +0530

    Update metasploit.sh

--- metasploit.sh
+++ metasploit.sh
@@ -8,24 +8,24 @@
 clear
 sleep 0.5
 echo -e '\033[1;94m
-                            ┏━┳━┓ ┏┓   ┏━┓     ┏┳┓
-                            ┃┃┃┃┣━┫┗┳━┓┃━╋━┳┓┏━╋┫┗┓
-                            ┃┃┃┃┃┻┫┏┫╋┗╋━┃╋┃┗┫╋┃┃┏┫
-                            ┗┻━┻┻━┻━┻━━┻━┫┏┻━┻━┻┻━┛
-                                         ┗┛'
-echo -e '\033[1;92m                             ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ '
-echo -e '\033[1;91m                             ┏━━┓  ┏━┳┓
-                             ┗┃┃╋━┳┫━┫┗┳━┓┏┓┏┓┏━┳┳┓
-                             ┏┃┃┫┃┃┣━┃┏┫╋┗┫┗┫┗┫┻┫┏┛
-                             ┗━━┻┻━┻━┻━┻━━┻━┻━┻━┻┛\033[0m'
+┏━┳━┓ ┏┓   ┏━┓     ┏┳┓
+┃┃┃┃┣━┫┗┳━┓┃━╋━┳┓┏━╋┫┗┓
+┃┃┃┃┃┻┫┏┫╋┗╋━┃╋┃┗┫╋┃┃┏┫
+┗┻━┻┻━┻━┻━━┻━┫┏┻━┻━┻┻━┛
+               ┗┛'
+echo -e '\033[1;92m▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ '
+echo -e '\033[1;91m┏━━┓  ┏━┳┓
+┗┃┃╋━┳┫━┫┗┳━┓┏┓┏┓┏━┳┳┓
+┏┃┃┫┃┃┣━┃┏┫╋┗┫┗┫┗┫┻┫┏┛
+┗━━┻┻━┻━┻━┻━━┻━┻━┻━┻┛\033[0m'
 sleep 0.2
 echo
-printf "\e[1;94m                             [\e[0m\e[1;77m1\e[0m\e[1;94m]\e[0m\e[1;94m Install Metasploit"
+printf "\e[1;94m[\e[0m\e[1;77m1\e[0m\e[1;94m]\e[0m\e[1;94m Install Metasploit"
 echo
 echo
-printf "\e[1;92m                            [\e[0m\e[1;77m2\e[0m\e[1;92m]\e[0m\e[1;92m Uninstall Metasploit\e[0m"
+printf "\e[1;92m[\e[0m\e[1;77m2\e[0m\e[1;92m]\e[0m\e[1;92m Uninstall Metasploit\e[0m"
 echo
-printf $'\n\e[1;91m                             [\e[0m\e[1;77m*\e[0m\e[1;91m] Choose an option: \e[0m'
+printf $'\n\e[1;91m[\e[0m\e[1;77m*\e[0m\e[1;91m] Choose an option: \e[0m'
 read meta
 
 if [[ $meta = "1" ]] || [[ $meta = "01" ]]
@@ -39,6 +39,6 @@ cd .setup && bash uninstall.sh
 else
 echo
 sleep 1
-printf "\e[1;95m                        [!] Invalid option! ~Restarting..\e[0m\n"
+printf "\e[1;95m[!] Invalid option! ~Restarting..\e[0m\n"
 bash metasploit.sh
 fi
