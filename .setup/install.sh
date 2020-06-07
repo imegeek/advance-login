8fe9f30d2935b49c6f7d69c883e62d37d5e9aaa4
ABHacker Official <63346676+abhackerofficial@users.noreply.github.com>
2020-06-07 08:28:46 +0530

    Update install.sh

--- .setup/install.sh
+++ .setup/install.sh
@@ -5,7 +5,7 @@ fi
 if [ ! -d "/data/data/com.termux/files/usr/opt/metasploit-framework" ]; then
 clear
 command -v metasploit > /dev/null 2>&1 || dpkg --configure -a
-command -v metasploit > /dev/null 2>&1 || { echo -e >&2 "\033[1;91m[+] Metasploit-framework Installing...\033[1m" && echo;pkg install unstable-repo; pkg install metasploit -y;echo && echo -e '\033[1;94m[$] "msfconsole" to start!\033[0m';sleep 5; clear; connection="$(ping -c 1 -q www.google.com >&/dev/null; echo $?)"
+command -v metasploit > /dev/null 2>&1 || { echo -e >&2 "\033[1;91m[+] Metasploit-framework Installing...\033[1m" && echo;pkg install unstable-repo; pkg install metasploit -y;echo && echo -e '\033[1;94m[$] "msfconsole" to start!\033[0m';cd /data/data/com.termux/files/usr/opt/metasploit-framework/lib && rm -rf pg > /dev/null 2>&1;sleep 5; clear; connection="$(ping -c 1 -q www.google.com >&/dev/null; echo $?)"
 if [[ "$connection" != 0 ]]
 then clear
 echo -e "\033[1;91m                             [\033[1;92m-\033[1;91m] No Internet\033[1;92m connection!"
