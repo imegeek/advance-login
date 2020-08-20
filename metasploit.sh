f60b4d807d4ee896e64c756372aec186c47c5aa1
ABHacker Official <63346676+abhackerofficial@users.noreply.github.com>
2020-08-20 23:07:06 +0530

    Update metasploit.sh

--- metasploit.sh
+++ metasploit.sh
@@ -5,32 +5,51 @@
 # Script follows here:
 
 
+banner() {
 clear
 sleep 0.5
 echo -e '\033[1;94m
-┏━┳━┓ ┏┓   ┏━┓     ┏┳┓
-┃┃┃┃┣━┫┗┳━┓┃━╋━┳┓┏━╋┫┗┓
-┃┃┃┃┃┻┫┏┫╋┗╋━┃╋┃┗┫╋┃┃┏┫
-┗┻━┻┻━┻━┻━━┻━┫┏┻━┻━┻┻━┛
-             ┗┛'
-echo -e '\033[1;92m▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ '
-echo -e '\033[1;91m┏━━┓  ┏━┳┓
-┗┃┃╋━┳┫━┫┗┳━┓┏┓┏┓┏━┳┳┓
-┏┃┃┫┃┃┣━┃┏┫╋┗┫┗┫┗┫┻┫┏┛
-┗━━┻┻━┻━┻━┻━━┻━┻━┻━┻┛\033[0m'
-sleep 0.2
-echo
-printf "\e[1;94m[\e[0m\e[1;77m1\e[0m\e[1;94m]\e[0m\e[1;94m Install Metasploit"
-echo
-echo
-printf "\e[1;92m[\e[0m\e[1;77m2\e[0m\e[1;92m]\e[0m\e[1;92m Uninstall Metasploit \e[0m"
-echo
-echo
-printf "\e[1;38;2;255;255;0m[\e[0m\e[1;77m3\e[0m\e[1;38;2;255;255;0m]\e[0m\e[1;38;2;255;255;0m MSVENOM [Payload]\e[0m"
-echo
-printf $'\n\e[1;91m[\e[0m\e[1;77m*\e[0m\e[1;91m] Choose an option: \e[0m'
-read meta
+  ┏━┳━┓ ┏┓   ┏━┓     ┏┳┓
+  ┃┃┃┃┣━┫┗┳━┓┃━╋━┳┓┏━╋┫┗┓
+  ┃┃┃┃┃┻┫┏┫╋┗╋━┃╋┃┗┫╋┃┃┏┫
+  ┗┻━┻┻━┻━┻━━┻━┫┏┻━┻━┻┻━┛
+               ┗┛'
+echo -e '\033[1;92m  ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ '
+echo -e '\033[1;91m
+  ┏━━┓  ┏━┳┓
+  ┗┃┃╋━┳┫━┫┗┳━┓┏┓┏┓┏━┳┳┓
+  ┏┃┃┫┃┃┣━┃┏┫╋┗┫┗┫┗┫┻┫┏┛
+  ┗━━┻┻━┻━┻━┻━━┻━┻━┻━┻┛
 
+\e[0;4m—Coding by @abhackerofficial—\033[0m'
+sleep 0.2 ;echo; }
+
+options() {
+printf "\e[1;94m[\e[0m\e[1;77m1\e[0m\e[1;94m]\e[0m\e[1;94m Install Metasploit \e[0m(Installed\e[0m)"
+echo""
+printf "\e[1;92m[\e[0m\e[1;77m2\e[0m\e[1;92m]\e[0m\e[1;92m Uninstall Metasploit \e[0m"
+echo""
+printf "\e[1;38;2;255;255;0m[\e[0m\e[1;77m3\e[0m\e[1;38;2;255;255;0m]\e[0m\e[1;38;2;255;255;0m MSVENOM [Payload] \e[0m(MSF Required\e[0m)"
+echo""
+printf $'\n\e[1;91m[\e[0m\e[1;77m*\e[0m\e[1;91m] Choose an option: \e[0m'; }
+
+options1() {
+printf "\e[1;94m[\e[0m\e[1;77m1\e[0m\e[1;94m]\e[0m\e[1;94m Install Metasploit \e[0m(Not Installed\e[0m)"
+echo""
+printf "\e[1;92m[\e[0m\e[1;77m2\e[0m\e[1;92m]\e[0m\e[1;92m Uninstall Metasploit \e[0m"
+echo""
+printf "\e[1;38;2;255;255;0m[\e[0m\e[1;77m3\e[0m\e[1;38;2;255;255;0m]\e[0m\e[1;38;2;255;255;0m MSVENOM [Payload] \e[0m(MSF Required\e[0m)"
+echo
+printf $'\n\e[1;91m[\e[0m\e[1;77m*\e[0m\e[1;91m] Choose an option: \e[0m'; }
+
+if [ -d "/data/data/com.termux/files/usr/opt/metasploit-framework" ]; then
+banner;options
+else
+banner
+options1
+fi
+
+read meta
 if [[ $meta = "1" ]] || [[ $meta = "01" ]]
         then clear
 cd .setup && bash install.sh
@@ -41,9 +60,7 @@ cd .setup && bash uninstall.sh
 
 elif [[ $meta = "3" ]] || [[ $meta = "03" ]]
         then cd .setup && bash extra.sh
-else
-echo
-printf "\e[1;95m[!] Invalid option! ~Restarting..\e[0m\n"
-sleep 1
-bash metasploit.sh
+else echo;
+printf "\e[1;77m[!] Invalid option! ~Closing..\e[0m\n\n"
+exec sleep 0.5
 fi
