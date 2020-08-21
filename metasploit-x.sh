f9bee6fdd0111b7bed1171a652f6ce0455c4a0af
ABHacker Official <63346676+abhackerofficial@users.noreply.github.com>
2020-08-21 17:45:26 +0530

    Update metasploit-x.sh

--- metasploit-x.sh
+++ metasploit-x.sh
@@ -25,20 +25,24 @@ sleep 0.2 ;echo;echo; }
 options() {
 printf "\e[1;94m[\e[0m\e[1;77m1\e[0m\e[1;94m]\e[0m\e[1;94m Install Metasploit \e[0m(Installed\e[0m)"
 echo""
-printf "\e[1;92m[\e[0m\e[1;77m2\e[0m\e[1;92m]\e[0m\e[1;92m Uninstall Metasploit \e[0m"
+printf "\e[1;92m[\e[0m\e[1;77m2\e[0m\e[1;92m]\e[0m\e[1;92m Uninstall Metasploit \e[0m(Installed\e[0m"
 echo""
 printf "\e[1;38;2;255;255;0m[\e[0m\e[1;77m3\e[0m\e[1;38;2;255;255;0m]\e[0m\e[1;38;2;255;255;0m MSVENOM [Payload] \e[0m(MSF Required\e[0m)"
 echo""
-printf $'\n\e[1;91m[\e[0m\e[1;77m*\e[0m\e[1;91m] Choose an option: \e[0m'; }
+printf "\e[1;38;2;0;230;118m[\e[0m\e[1;77m4\e[0m\e[1;38;2;0;230;118m]\e[0m\e[1;38;2;0;230;118m Update \e[0m(Metasploit-X)\e[0m"
+echo
+printf $'\n\e[1;91m[\e[0m\e[1;77m*\e[0m\e[1;91m] Choose an option : \e[0m'; }
 
 options1() {
 printf "\e[1;94m[\e[0m\e[1;77m1\e[0m\e[1;94m]\e[0m\e[1;94m Install Metasploit \e[0m(Not Installed\e[0m)"
 echo""
-printf "\e[1;92m[\e[0m\e[1;77m2\e[0m\e[1;92m]\e[0m\e[1;92m Uninstall Metasploit \e[0m"
+printf "\e[1;92m[\e[0m\e[1;77m2\e[0m\e[1;92m]\e[0m\e[1;92m Uninstall Metasploit \e[0m(Not Installed)\e[0m"
 echo""
 printf "\e[1;38;2;255;255;0m[\e[0m\e[1;77m3\e[0m\e[1;38;2;255;255;0m]\e[0m\e[1;38;2;255;255;0m MSVENOM [Payload] \e[0m(MSF Required\e[0m)"
 echo
-printf $'\n\e[1;91m[\e[0m\e[1;77m*\e[0m\e[1;91m] Choose an option: \e[0m'; }
+printf "\e[1;38;2;0;230;118m[\e[0m\e[1;77m4\e[0m\e[1;38;2;0;230;118m]\e[0m\e[1;38;2;0;230;118m Update \e[0m(Metasploit-X)\e[0m"
+echo
+printf $'\n\e[1;91m[\e[0m\e[1;77m*\e[0m\e[1;91m] Choose an option : \e[0m'; }
 
 if [ -d "/data/data/com.termux/files/usr/opt/metasploit-framework" ]; then
 banner;options
@@ -58,6 +62,10 @@ cd .setup && bash uninstall.sh
 
 elif [[ $meta = "3" ]] || [[ $meta = "03" ]]
         then cd .setup && bash extra.sh
+
+elif [[ $meta = "3" ]] || [[ $meta = "03" ]]
+        then echo -e "\e[0m[!] Updating... Metasploit-X"
+cd .setup && bash update.sh
 else echo;
 printf "\e[1;77m[!] Invalid option! ~Closing..\e[0m\n\n"
 exec sleep 0.5
